74f151c4ff5eed68a1522b7324fe3993297f395e
Myron Kalaitzakis <kalaitzzakis@gmail.com>
2025-11-12 03:44:26 +0100

    clean up the trash

--- using-collections-ex1.py
+++
@@ -1,6 +0,0 @@
-# Write Python code to print the seventh number of range(0, 25, 3).
-
-my_range = range(0, 25, 3) # creating a variable named my_range
-
-print(my_range[6]) # since indexing is 0-based, printing the seventh element will use the index 6
-
